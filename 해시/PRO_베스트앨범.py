d34762a3a52f9193db04b66c0092d573bba1e4ee
이주영 <ph2978@naver.com>
2022-05-17 00:02:29 +0900

    220517

--- 해시/PRO_베스트앨범.py
+++ 해시/PRO_베스트앨범.py
@@ -5,12 +5,16 @@ def solution(genres, plays):
         for i in range(len(genre)):
             if genre[i][0] == genres[g]:
                 genre[i][1].append(g)
+                genre[i][2] += plays[g]
                 break
         else:
-            genre.append([genres[g], [g]])
-    genre.sort(key=lambda x: -len(x[1]))
-
-
+            genre.append([genres[g], [g], plays[g]])
+    # 많이 들은 장르 순으로 정렬
+    genre.sort(key=lambda x: -x[2])
+    # 장르 안에서 많이 들은 노래 순으로 정렬해 앞에 2곡만 answer에 저장
+    for lst in genre:
+        lst[1].sort(key=lambda x: -plays[x])
+        answer += lst[1][:2]
 
     return answer
 
